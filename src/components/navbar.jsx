c0f68ff47fde6f935ff9d8dbb14cb33bec135fa0
Eddie Jaquez <edsmacbook@gmail.com>
2019-11-09 23:17:28 -0500

    updated

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,26 +7,26 @@ import {
   Button
   //   FormGroup
 } from "react-bootstrap";
-import Logo from "./images/newlogo.png";
+import Logo from "./images/logo.png";
 
 class Navibar extends Component {
   render() {
     return (
       <div>
-        <Navbar bg="dark" variant="dark">
+        <Navbar bg="secondary" variant="dark">
           <Navbar.Brand href="#home">
-            <h1 id="logo">Mr.Eddie</h1>
+            <img src={Logo} width="15%" />
           </Navbar.Brand>
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#Boro">Boro</Nav.Link>
-            <Nav.Link href="#InstaGram">InstaGram</Nav.Link>
+            <Nav.Link href="#Home">Home</Nav.Link>
+            <Nav.Link href="#About">Features</Nav.Link>
+            <Nav.Link href="#Portfolio">Boro</Nav.Link>
+            <Nav.Link href="#Contact">InstaGram</Nav.Link>
           </Nav>
-          <Form inline>
-            {/* <FormControl type="text" placeholder="Search" className="mr-sm-2" /> */}
-            <Button variant="outline-light mr-3">Log In</Button>
-          </Form>
+          {/* <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-light mr-5">Log In</Button>
+          </Form> */}
         </Navbar>
       </div>
     );
